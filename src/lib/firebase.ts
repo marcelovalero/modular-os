e20e0dd358407dc16e2de264a2e6bf32b4fcde17
marcelovalero <marcelo.svalero@gmail.com>
2026-02-11 23:02:19 +0000

    MVP: Conexão Firebase funcionando e Estrutura Limpa

---
+++ src/lib/firebase.ts
@@ -0,0 +1,18 @@
+
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCt0i4YfmNf0bSJBYLCvKZcuejrST5fXk8",
+  authDomain: "modular-os.firebaseapp.com",
+  projectId: "modular-os",
+  storageBucket: "modular-os.appspot.com",
+  messagingSenderId: "995088201685",
+  appId: "1:995088201685:web:3e1808b0c160d144156f5c"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+
+export { app, db };
---
+++ src/types/obra.ts
@@ -0,0 +1,8 @@
+
+export interface Obra {
+  id: string;
+  titulo: string;
+  // Adicione outros campos conforme necessário
+  // descricao?: string;
+  // imageUrl?: string;
+}
